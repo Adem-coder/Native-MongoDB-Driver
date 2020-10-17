da53a60b1f8ae52a998615e688aa326102430c09
Ashu <ashutolosa3@gmail.com>
2020-10-18 00:39:00 +0300

    Add multiple data in a database by using mongoose

--- app.js
+++ app.js
@@ -17,7 +17,47 @@ const fruit = new Fruit ({
     review: "Pretty solid as a fruit."
 });
 
-fruit.save();
+// fruit.save();
+
+const personSchema = new mongoose.Schema ({
+    name: String,
+    age: Number
+    
+}); 
+// mongoose model
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person ({
+    name: "John",
+    age: 37
+    
+});
+
+person.save();
+
+const kiwi = new Fruit ({
+    name: "Kiwi",
+    score: 10,
+    review: "The best fruit!"
+});
+const banana = new Fruit({
+    name: "Banana",
+    score: 7,
+    review: "Has a smooth feeling"
+});
+const orange = new Fruit({
+    name: "Orange",
+    score: 6,
+    review: "Better than most"
+});
+
+Fruit.insertMany([kiwi, orange, banana], function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Succesfully saved all the fruits to fruitsDB");
+    }
+});
 
 const findDocuments = function(db, callback){
     // Get the documents collection
